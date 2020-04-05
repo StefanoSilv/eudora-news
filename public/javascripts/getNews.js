7b9888da0be04b647317631079c8c2ea9299d162
Stefano Silvestrini <silver.stefano@gmail.com>
2020-04-05 16:38:41 +0200

    keywords inserted for searching the news

--- public/javascripts/getNews.js
+++ public/javascripts/getNews.js
@@ -6,11 +6,63 @@ let dd = String(today.getDate()).padStart(2, "0");
 let mm = String(today.getMonth() + 1).padStart(2, "0");
 let lastyyyy = (Number(today.getFullYear()) - 1).toString();
 let lastyear = new Date(`${lastyyyy}-${mm}-${dd}`);
-console.log(today);
+let sostenibilità = [
+  "sostenibile",
+  "sostenibilità",
+  "ecosostenibile",
+  "eco-sostenibile",
+  "ecologia",
+  "ecologico",
+  "ecologica",
+  "(blue growth)",
+  "(Gunther Pauli)"
+];
+let natura = [
+  "organico",
+  "organica",
+  "natura",
+  "naturale",
+  "(metodo narutale)",
+  "biologico",
+  "biologica",
+  "biologia",
+  "(kilometro zero)",
+  "(KM 0)",
+  "(KM zero)",
+  "(decrescita felice)",
+  "canapa"
+];
+
+let people = [
+  "(Gunther Pauli)",
+  "(Umberto Galimberti)",
+  "(Paolo Silvestrini)",
+  "(Giuseppe Ferraro)",
+  "(Anthony Legget)",
+  "(Anthony James Legget)"
+];
+
+let mind = ["consapevolezza", "felicità", "(nuova visione)", "comunità"];
+
+let argomenti = [
+  "(fisica quantistica)",
+  "(intelligenza artificiale)",
+  "(crescita sostenibile)"
+];
+
+const getString = arrays => {
+  let q = "(" + arrays[0].join(" OR ") + ")";
+  arrays.shift();
+  arrays.forEach(array => {
+    q += " OR " + "(" + array.join(" OR ") + ")";
+  });
+  console.log(q);
+  return q;
+};
 
 newsapi.v2
   .everything({
-    q: "bitcoin",
+    qInTitle: getString([argomenti, mind, natura, sostenibilità]),
     from: `${lastyear}`,
     to: `${today}`,
     language: "it",
@@ -19,7 +71,7 @@ newsapi.v2
     pageSize: 100
   })
   .then(response => {
-    // console.log(response);
+    console.log(response);
     /*
     {
       status: "ok",
